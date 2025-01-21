1dced39854339caa95e1cec35e0b5b244c02e379
BarbeXceed <barberiepbe@gmail.com>
2025-01-21 16:33:26 +0100

    Déplacement du fichier ainsi que la création de la fonction genCharArray

--- src/newGen.cpp
+++ src/newGen.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
 
@@ -30,7 +31,6 @@ double randomDouble(int min, int max){
 
 }
 
-
 /**
  * Mélange le tableau, PEUT NE PAS ETRE EFFICACE
  * Complexité en temps : O(n)
@@ -57,21 +57,67 @@ void displayArray(vector<T> tab){
 
     int n = tab.size();
     for(int i = 0; i < n; i++){
-        cout << tab[i] << endl;
+        cout << "Indice " << i << " : " << tab[i] << endl;
     }
 };
 
+vector<int> genIntArray(int n){
+	vector<int> res;
+	for(int i = 0; i < n; i++){
+		res.push_back(randomInt(0, 10*n));
+	}
+
+	res.shrink_to_fit();
+	return res;
+}
+
+
+vector<float> genFloatArray(int n){
+	vector<float> res;
+	for(int i = 0; i < n; i++){
+		res.push_back((float) randomDouble(0, 10*n));
+	}
+	res.shrink_to_fit();
+	return res;
+}
+
+vector<double> genDoubleArray(int n){
+	vector<double> res;
+	for(int i = 0; i < n; i++){
+		res.push_back(randomDouble(0, 10*n));
+	}
+	res.shrink_to_fit();
+	return res;
+}
+
+vector<char> genCharArray(int n){
+	vector<char> res;
+	vector<char> charTab;
+	//Obtention de la taille du fichier
+	int sizeCharTab = 0;
+	string ligne;
+	ifstream file("letter.txt");
+	while (getline (file, ligne)) {
+		charTab.push_back(ligne[0]);
+		sizeCharTab++;
+	}
+	file.close();
+
+	//Remplissage du tableau
+	for(int i = 0; i < n; i++){
+		int randomIndex = randomInt(0, sizeCharTab-1);
+		res.push_back(charTab[randomIndex]);
+	}
+
+	res.shrink_to_fit();
+	return res;
+}
 
 int main(){
 
 	srand(time(NULL));
 
-    vector<string> myVector = {"Venom", "Ky", "Sol", "Slayer", "Johny"};
-	cout << "--- Avant Affichage ---" << endl;
+    vector<char> myVector = genCharArray(100);
 	displayArray(myVector);
-	cout << "--- Après 1er affichage ---" << endl;
-	shuffle(&myVector);
-	cout << "--- Après mélange ---" << endl;
-	displayArray(myVector);
-	cout << "---Après 2nd affichage ---" << endl;
+
 }
