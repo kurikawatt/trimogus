fb9036663e63cf795779e40691cb92d754d32f93
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-21 15:46:41 +0100

    Déplacement des fonctions dans leurs headers, ça fonctionne comme ça

--- src/tools/checkers.h
+++ src/tools/checkers.h
@@ -4,5 +4,12 @@
 
 using namespace std;
 
-template <typename T> 
-bool is_vector_sorted(vector<T> vec, bool reversed=false);
+template <typename T>
+bool is_vector_sorted(vector<T> vec, bool reversed=false){
+    for (int i = 0; i < vec.size() - 1; i++){
+        if ((!reversed && (vec[i] > vec[i+1])) || (reversed && (vec[i] < vec[i+1]))){
+            return false;
+        }
+    }
+    return true;
+};
--- src/tools/printers.h
+++ src/tools/printers.h
@@ -7,7 +7,17 @@
 using namespace std;
 
 template <typename T> 
-void print_array(T arr[], size_t size, string separator=" ");
+void print_array(T arr[], size_t size, string separator=" "){
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << separator;
+    }
+    cout << endl;
+};
 
 template <typename T> 
-void print_vector(vector<T> vec, string separator=" ");
+void print_vector(vector<T> vec, string separator=" "){
+    for (T element : vec){
+        cout << element << separator;
+    }
+    cout << endl;
+};
