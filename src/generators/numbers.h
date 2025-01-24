98a2ddda11389042a3f382b8bf7500ef877fce74
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-24 19:57:38 +0100

    En-têtes pour les générateurs numériques

---
+++ src/generators/numbers.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+int random_int(int a, int b);
+
+float random_float(float a, float b);
+
+double random_double(double a, double b);
+
+vector<int> random_int_vector(int a, int b, size_t size);
+
+vector<float> random_float_vector(float a, float b, size_t size);
+
+vector<double> random_double_vector(double a, double b, size_t size);
