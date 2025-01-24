e0f59bea24640526aa77aa79f1bced587e849210
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-24 14:23:38 +0100

    implémentation du tri selection

--- src/sorts/selection.h
+++ src/sorts/selection.h
@@ -1,6 +1,20 @@
 #pragma once
 
 #include <vector>
+#include "tools/swap.h"
 
 template <typename T>
-void selection_sort(vector<T> vec);
+void selection_sort(vector<T> vec){
+    int min_index;
+    for (int i = 0; i < vec.size(); i++){
+        min_index = i;
+        for (int j = i+1; j < vec.size(); j++){
+            if (vec[j] < vec[i]){
+                min_index = j;
+            }
+        }
+        if (i != min_index){
+            swap(vec, i, min_index);
+        }
+    }
+};
