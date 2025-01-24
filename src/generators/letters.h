4fd7433f17ac8192dc69fe4e90f27039707ff7ca
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-24 20:02:28 +0100

    En-têtes pour les générateurs lettres/chaine de lettres/mots

---
+++ src/generators/letters.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+char random_char();
+
+string random_string(size_t size);
+
+string random_word(vector<string> words);
+
+vector<char> random_char_vector(size_t size);
+
+vector<string> random_string_vector(size_t size);
+
+vector<string> random_words_vector(size_t size);
