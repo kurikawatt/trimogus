ede57ac83b881f14ed5996bb4e688472d1c193fd
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-24 15:29:26 +0100

    swap reçoit un ptr de vector

--- src/tools/swap.h
+++ src/tools/swap.h
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-template <typename T> void swap(vector<T> vec, unsigned int i, unsigned int j){
-    T temp = vec[i];
-    vec[i] = vec[j];
-    vec[j] = temp;
-} 
+template <typename T> void swap(vector<T> *vec, unsigned int i, unsigned int j){
+    T temp = (*vec)[i];
+    (*vec)[i] = (*vec)[j];
+    (*vec)[j] = temp;
+}
