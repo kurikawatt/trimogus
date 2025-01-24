3e80d9ed06056cfb7459e3a0df012a39ce84c63b
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-24 17:36:18 +0100

    traitement des arguments type -<lettre> dans le main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <string>
+#include <unistd.h>
 
 using namespace std;
 
+void print_version(){
+    cout << "Trimogus InDev" << endl;
+}
 
-int main(){
-    cout << "Trimogus vous salue!" << endl;
+int main(int argc, char *argv[]){
+
+    int opt;
+
+    while ( (opt = getopt(argc, argv, "v")) != -1){
+        switch (opt){
+            case 'v':
+                print_version();
+                break;
+            default:
+                break;
+        }
+    }
+
     return 0;
 }
