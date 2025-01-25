80f1e7fc181058f1aad61864028a66b010b196bd
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-25 10:53:13 +0100

    correction d'une coquille dans knuth_shuffle()

--- src/tools/shufflers.h
+++ src/tools/shufflers.h
@@ -10,7 +10,7 @@ using namespace std;
 template <typename T>
 void hnuth_shuffle(vector<T> vec){
     int j;
-    for (int i = 0; i < vec.size(; i++)){
+    for (int i = 0; i < vec.size(); i++){
         j = random_int(0, vec.size()-1);
         if (i != j) swap(vec, i, j);
     }
