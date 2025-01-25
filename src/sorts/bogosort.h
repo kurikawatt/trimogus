0a778759986c148cd1fc4a7ff865d65f521067b0
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-25 11:05:39 +0100

    fix du bogosort

--- src/sorts/bogosort.h
+++ src/sorts/bogosort.h
@@ -10,6 +10,6 @@ using namespace std;
 template <typename T>
 void bogosort(vector<T> vec, bool reversed=false){
     while (!is_vector_sorted(vec, reversed)){
-        knuth_shuffle(vec, reversed);
+        knuth_shuffle(vec);
     }
 }
