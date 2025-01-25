c232b71a9950f6b445b393f8388dc912131ea45d
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-25 11:13:36 +0100

    fix : on passe la référence du vec

--- src/sorts/bogosort.h
+++ src/sorts/bogosort.h
@@ -8,7 +8,7 @@
 using namespace std;
 
 template <typename T>
-void bogosort(vector<T> vec, bool reversed=false){
+void bogosort(vector<T> &vec, bool reversed=false){
     while (!is_vector_sorted(vec, reversed)){
         knuth_shuffle(vec);
     }
