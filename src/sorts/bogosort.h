7934609462bd32d302acb84a1bd80171bf429ea3
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-25 10:52:48 +0100

    implémentation du bogosort (ie. tri stupide)

---
+++ src/sorts/bogosort.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+#include "tools/shufflers.h"
+#include "tools/checkers.h"
+
+using namespace std;
+
+template <typename T>
+void bogosort(vector<T> vec, bool reversed=false){
+    while (!is_vector_sorted(vec, reversed)){
+        knuth_shuffle(vec, reversed);
+    }
+}
