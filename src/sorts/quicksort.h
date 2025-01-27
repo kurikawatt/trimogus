a59b28bfeca7e2f59e1daf02d30ea8c59fee8b2f
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-27 14:59:49 +0100

    Passage du quicksort.cpp en fichier header utilisable, retrait du superflu

---
+++ src/sorts/quicksort.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+int partition(vector<T>& arr, int low, int high) {
+  
+    T pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+
+template <typename T>
+void quickSort(vector<T>& arr, int low, int high) {
+  
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
