291b7cedaa3a0ae1cd48ea3f7651254c48e4fa72
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-27 15:02:07 +0100

    Passage du insertion.cpp en fichier header utilisable, retrait du superflu

---
+++ src/sorts/insertion.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+void insertion(vector<T>& tab){
+    
+    for (size_t i = 0; i < tab.size(); i++){
+        T k = tab[i];
+        int j = i-1;
+
+        while(j>= 0 && tab[j] >  k){
+            tab[j+1] = tab[j];
+            j=j-1;
+        }
+
+        tab[j+1]= k;
+    }
+};
