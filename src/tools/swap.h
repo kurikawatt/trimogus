7e64f5f2afbba3691208e9efe5fedf73b09b9795
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-28 07:49:41 +0100

    Ajout d'un compteur de swap

--- src/tools/swap.h
+++ src/tools/swap.h
@@ -4,7 +4,10 @@
 
 using namespace std;
 
+int __SWAP_COUNT__ = 0;
+
 template <typename T> void swap(vector<T> &vec, unsigned int i, unsigned int j){
+    __SWAP_COUNT__++;
     T temp = vec[i];
     vec[i] = vec[j];
     vec[j] = temp;
