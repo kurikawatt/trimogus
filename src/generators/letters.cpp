3ce51f65510f0433fe411722b166c6962387d34e
BarbeXceed <barberiepbe@gmail.com>
2025-01-28 15:48:20 +0100

    Correction typo

--- src/generators/letters.cpp
+++ src/generators/letters.cpp
@@ -15,6 +15,7 @@ string random_string(size_t size){
 string random_word(vector<string> &words){
     return "random";
 }
+
 vector<char> random_char_vector(size_t size){
     vector<char> c;
     for (int i = 0; i < size; i++){
@@ -35,7 +36,7 @@ vector<string> random_string_vector(size_t size){
     return c;
 }
 
-vector<string> random_string_vector(vector<string> &words, size_t size){
+vector<string> random_words_vector(vector<string> &words, size_t size){
     vector<string> c;
     for (int i = 0; i < size; i++){
         c.push_back(random_word(words));
