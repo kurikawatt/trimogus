412c7138face55a727c0fa81f27b6da1a17fa80d
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-28 14:35:19 +0100

    fix du tri bulle

--- src/sorts/bublesort.h
+++ src/sorts/bublesort.h
@@ -10,9 +10,10 @@ using namespace std;
 template <typename T>
 void bublesort(vector<T> &vec, bool reversed=false){
     for (int i = 0; i < vec.size()-1; i++){
-        for (int j = 0; j < i-1; j++){
-            if (vec[j+1] < vec[j]){
-                swap(vec, j, j+1);
+        for (int j = 0; j < vec.size()-i-1; j++){
+            __COMPARISION_COUNT__++;
+            if ((reversed && vec[j] < vec[j+1]) || (!reversed && vec[j] > vec[j+1])){
+                swap(vec[j], vec[j+1]);
             }
         }
     }
