43d0614311a2c2203e2ad20bc84b1a1f102130c4
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-28 15:18:53 +0100

    fix des bornes

--- src/generators/numbers.h
+++ src/generators/numbers.h
@@ -5,15 +5,18 @@
 #include <vector>
 #include <cstdlib>
 
+#define MIN_INT_GEN -1000000
+#define MAX_INT_GEN 1000000
+
 using namespace std;
 
-int random_int(int a=INT_MIN, int b=INT_MAX);
+int random_int(int a=MIN_INT_GEN, int b=MAX_INT_GEN);
 
 float random_float(float a, float b);
 
 double random_double(double a, double b);
 
-vector<int> random_int_vector(size_t size, int a=INT_MIN, int b=INT_MAX);
+vector<int> random_int_vector(size_t size, int a=MIN_INT_GEN, int b=MAX_INT_GEN);
 
 vector<float> random_float_vector(size_t size, float a, float b);
 
