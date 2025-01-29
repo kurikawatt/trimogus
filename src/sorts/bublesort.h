4db5cd17320e9b73b1f43c8aa910fed6516f6f5b
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-01-29 11:52:39 +0100

    fix out of bound causing segv

--- src/sorts/bublesort.h
+++ src/sorts/bublesort.h
@@ -9,11 +9,12 @@ using namespace std;
 
 template <typename T>
 void bublesort(vector<T> &vec, bool reversed=false){
+    if (vec.size() < 2) return;
     for (int i = 0; i < vec.size()-1; i++){
         for (int j = 0; j < vec.size()-i-1; j++){
             __COMPARISION_COUNT__++;
             if ((reversed && vec[j] < vec[j+1]) || (!reversed && vec[j] > vec[j+1])){
-                swap(vec[j], vec[j+1]);
+                probed_swap(vec, vec[j], vec[j+1]);
             }
         }
     }
