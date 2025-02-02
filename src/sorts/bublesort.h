d2720df48345200567688edec1ec98a7d1fda730
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-02-02 11:19:46 +0100

    optimized version of the bubblesort

--- src/sorts/bublesort.h
+++ src/sorts/bublesort.h
@@ -18,3 +18,21 @@ void bubblesort(vector<T> &vec, bool reversed=false){
         }
     }
 }
+
+template <typename T>
+void optimized_bubblesort(vector<T> &vec, bool reversed=false) {
+    bool swapped;
+    for (size_t i = 0; i < vec.size()-1; i++) {
+        swapped = false;
+        for (size_t j = 0; j < vec.size()-i-1; j++) {
+            __COMPARISION_COUNT__++;
+            if ((reversed && vec[j] < vec[j+1]) || (!reversed && vec[j] > vec[j+1])) {
+                probed_swap(vec, j, j+1);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
