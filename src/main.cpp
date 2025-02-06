b133f2ae712392a3dc10a6edeee0e8a74ae5d976
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-02-06 13:34:36 +0100

    added mergesort to main benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,14 @@ int main(int argc, char *argv[]){
     // Setting the seed to an unknown value
     srand(time(NULL));
 
-    size_t vec_max_size = 600000;
+    size_t vec_max_size = 35000;
 
     vector<BenchUnit<int>> benchs = {};
     benchs.push_back(BenchUnit<int>("selection sort", selection_sort, vec_max_size));
     benchs.push_back(BenchUnit<int>("bubble sort", bubblesort, vec_max_size));
     benchs.push_back(BenchUnit<int>("quicksort", quick_sort, vec_max_size));
+    benchs.push_back(BenchUnit<int>("top down mergesort", topdown_mergesort, vec_max_size));
+    benchs.push_back(BenchUnit<int>("bottom up mergesort", bottomup_mergesort, vec_max_size));
 
     for (BenchUnit<int> b : benchs){
         b.dirty_run();
