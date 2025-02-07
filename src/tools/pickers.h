21ad6e09f2b9e8885b61981a96826195ee252dcf
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-02-07 10:19:39 +0100

    Documentation de tools/pickers.h

--- src/tools/pickers.h
+++ src/tools/pickers.h
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+/**
+ * Pioche un élément du vecteur
+ * 
+ * @param vec le vecteur de type T dans lequel on pioche
+ * 
+ * @return v de type T
+ * @ensures v est une valeur de vec
+ */
 template <typename T>
 T pick_in_vector(vector<T> &vec){
     int i = random_int(0, vec.size()-1);
