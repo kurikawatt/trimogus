5a0621c3999c33737b89adf94e05a53c734de7c8
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-02-07 10:22:58 +0100

    Documentation de tools/printers.h

--- src/tools/printers.h
+++ src/tools/printers.h
@@ -6,19 +6,33 @@
 
 using namespace std;
 
+/**
+ * Afficher un tableau dans la sortie standard
+ * 
+ * @param arr[] un tableau de type T
+ * @param size la taille du tableau
+ * @requires size >= la taille réelle du tableau
+ * @param sperator la chaine de séparation entre deux valeurs
+ */
 template <typename T> 
 void print_array(T arr[], size_t size, string separator=" "){
-    for (int i = 0; i < size; i++){
+    for (size_t i = 0; i < size; i++){
         cout << arr[i] << separator;
     }
     cout << endl;
 };
 
+
+/**
+ * Afficher un vecteur dans la sortie standard
+ * 
+ * @param vec un vecteur de type T
+ * @param sperator la chaine de séparation entre deux valeurs
+ */
 template <typename T> 
 void print_vector(vector<T> vec, string separator=" "){
-    cout << "[";
     for (T element : vec){
         cout << element << separator;
     }
-    cout << "]" << endl;
+    cout << endl;
 };
