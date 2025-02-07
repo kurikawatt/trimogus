2f99c6d911dcae97a4dcf11ccb7d3bc88bad9186
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-02-07 10:28:15 +0100

    Documentation de tools/shufflers.h

--- src/tools/shufflers.h
+++ src/tools/shufflers.h
@@ -7,11 +7,20 @@
 
 using namespace std;
 
+/**
+ * Mélange en place un vecteur, 
+ * en utilisant la méthode Knuth (ou Fisher–Yates)
+ * 
+ * @param vec le vecteur à mélanger
+ * 
+ * @return /
+ * @ensures l'état du vecteur est une permutation possible
+ */
 template <typename T>
 void knuth_shuffle(vector<T> &vec){
     int j;
     for (int i = 0; i < vec.size(); i++){
         j = random_int(0, vec.size()-1);
-        if (i != j) swap(vec, i, j);
+        if (i != j) probed_swap(vec, i, j);
     }
 };
