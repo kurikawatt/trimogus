f31e9c3c920c1fc98349c7e1c4e40aa4340fb0d1
François "Kurikawa" Odin <francois@kurikawa.fr>
2025-02-07 10:16:00 +0100

    Documentation de tools/checkers.h

--- src/tools/checkers.h
+++ src/tools/checkers.h
@@ -4,10 +4,21 @@
 
 using namespace std;
 
+/**
+ * Vérifie si un vecteur est trié
+ * 
+ * @param vec un vecteur de type T
+ * @param reversed si false, vérifie que le vecteur est trié dans l'ordre croissant
+ *                 sinon, vérifie que le vecteur est trié dans l'ordre décroissant
+ * @return true si trié, false sinon
+ */
 template <typename T>
 bool is_vector_sorted(vector<T> vec, bool reversed=false){
     for (int i = 0; i < vec.size() - 1; i++){
-        if ((!reversed && (vec[i] > vec[i+1])) || (reversed && (vec[i] < vec[i+1]))){
+        if (
+            (!reversed && (vec[i] > vec[i+1])) // Vérification de l'ordre croissant
+            || (reversed && (vec[i] < vec[i+1])) // Vérification de l'ordre décroissant
+        ){
             return false;
         }
     }
