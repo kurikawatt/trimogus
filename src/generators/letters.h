13be042e1f5f9b29dd9b3f393c24318466cac76f
BarbeXceed <barberiepbe@gmail.com>
2025-02-11 15:05:32 +0100

    Ajout des signatures

--- src/generators/letters.h
+++ src/generators/letters.h
@@ -19,6 +19,8 @@ vector<string> random_words_vector(vector<string> &words, size_t size);
 
 void nextWord(string &currWord);
 
+void previousWOrd(string &currWord);
+
 vector<char> random_char_vector_controlled(size_t size, float proc);
 
 vector<string> random_string_vector_controlled(size_t size, float proc);
@@ -28,3 +30,5 @@ vector<string> random_word_vector_controlled(size_t size, vector<string> &words,
 vector<char> reversed_sorted_char_vector(size_t size);
 
 vector<string> reversed_sorted_word_vector(size_t size, vector<string> listesMots);
+
+vector<string> reversed_sorted_string_vector(size_t size);
